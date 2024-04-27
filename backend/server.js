52dbbf7143a187a3e022cbb3199c5eda21afe51d
amit78kum <amkumar7488@gmail.com>
2024-04-27 11:02:35 +0530

    Cart A

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose"
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config.js'
+import cartRouter from "./routes/cartRoute.js";
 //app config
 const app=express();
 const port=4000;
@@ -22,6 +23,7 @@ app.use("/api/food",foodRouter)
 //app.use("/api/user",userRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 
 
