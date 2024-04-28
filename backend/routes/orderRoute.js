2719b9892b9fe2f5d86d55af039ffe6ab967da37
amit78kum <amkumar7488@gmail.com>
2024-04-28 07:05:38 +0530

    Display Order Page

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js"
-import { placeorder, verifyOrder } from "../controllers/orderController.js";
+import { placeorder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 
 const orderRouter=express.Router();
 
 orderRouter.post("/place",authMiddleware,placeorder);
-orderRouter.post("/verify",verifyOrder)
+orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/userorders",authMiddleware,userOrders)
 
 
 export default orderRouter;
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 const PlaceOrder = () => {
   const {getTotalCartAmount,token,food_list,cartItems,url}=useContext(StoreContext);
   const [data,setData]=useState({
@@ -48,6 +49,19 @@ const PlaceOrder = () => {
       alert("Error")
      }
   }
+  const navigate=useNavigate();
+  useEffect(()=>{
+  if(!token){
+    navigate('/cart')
+    alert("first sign in account ")
+
+  }else if(getTotalCartAmount()===0){
+    navigate('/cart');
+    alert("item not added in cart");
+  }
+
+  
+  },[token])
   return (
     <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
